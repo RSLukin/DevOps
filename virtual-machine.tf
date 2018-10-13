5aee259c42c6bf02ac7017844e5f9f30a0d4c31a
RSLukin <44001035+RSLukin@users.noreply.github.com>
2018-10-14 01:32:27 +0300

    Create virtual-machine.tf

---
+++ virtual-machine.tf
@@ -0,0 +1,45 @@
+resource "azurerm_virtual_machine" "test" {
+  name                  = "${var.prefix}-vm"
+  location              = "${azurerm_resource_group.main.location}"
+  resource_group_name   = "${azurerm_resource_group.main.name}"
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "Standard_F2"
+
+  # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
+  # NOTE: This may not be optimal in all cases.
+  delete_os_disk_on_termination = true
+
+  # This means the Data Disk Disk will be deleted when Terraform destroys the Virtual Machine
+  # NOTE: This may not be optimal in all cases.
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+   }
+
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "task1vm"
+    admin_username = "rlukin"
+    custom_data = "cloud-init-jenkins.txt"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+    path     = "/home/rlukin/.ssh/authorized_keys"
+    key_data = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
+    }
+  }
+
+  tags = "${var.tags}"
+}

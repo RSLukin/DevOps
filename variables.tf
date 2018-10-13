10b72de1bb3dfc60d2033abd0b8de269c2d6cbf3
RSLukin <44001035+RSLukin@users.noreply.github.com>
2018-10-14 02:49:07 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,20 @@
 variable "prefix" {
   default     = "task1"
-  description = "The Prefix used for all resources in this example"
+  description = "The Prefix used for all resources in this task"
 }
 
 variable "location" {
   default     = "West Europe"
-  description = "The Azure Region in which the resources in this example should exist"
+  description = "The Azure Region in which the resources in this task should exist"
+}
+
+variable "sshkey" {
+  default     = ""
+  description = "The ssh key of the user (~/.ssh/id_rsa.pub)"
 }
 
 variable "tags" {
   type        = "map"
   default     = {}
-  description = "Any tags which should be assigned to the resources in this example"
+  description = "Any tags which should be assigned to the resources in this task"
 }

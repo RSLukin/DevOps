9355cb8a758a77b060964a036a5a6c86d9ee4e65
RSLukin <44001035+RSLukin@users.noreply.github.com>
2018-10-17 18:07:06 +0300

    Update dependencies.tf

--- dependencies.tf
+++ dependencies.tf
@@ -36,7 +36,7 @@ resource "azurerm_network_interface" "main" {
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "myterraformnsg" {
-    name                = "myNetworkSecurityGroup"
+    name                = "${var.prefix}-NSG"
     location            = "${azurerm_resource_group.main.location}"
     resource_group_name = "${azurerm_resource_group.main.name}"
 
